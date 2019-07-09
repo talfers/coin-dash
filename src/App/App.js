f7f66c22ab790fa232f826581353f3350de7bf46
Taylor Alfers <Taylor>
2019-07-09 17:47:44 -0600

    app initial layout

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import WelcomeMessage from './WelcomeMessage';
+import './App.css';
+import AppLayout from './AppLayout';
+
+
+
+function App() {
+  return (
+    <AppLayout>
+      <WelcomeMessage/>
+    </AppLayout>
+  );
+}
+
+export default App;
